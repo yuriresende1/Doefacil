2a326017c850044e8ffa4f0d12134717df5fd05c
GabrielCaetano13 <gabriel011depaulac@gmail.com>
2023-06-07 16:58:49 -0300

    página de doações sendo inicializada

--- Doefacil/Views/donations.php
+++ Doefacil/Views/donations.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doações</title>
-    <link rel="stylesheet" href="../assets/css/cssdoefacil.css">
+    <link rel="stylesheet" href="../assets/css/donations.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -45,6 +45,11 @@
     <br>
     <input type="submit" value="Cadastrar">
   </form>
+  <div class="container">
+    <?php
+      include("../Controllers/Actions.php");
+    ?>
+  </div>
   <footer>
 		<h3>Atendimento</h3>
 		<br>
