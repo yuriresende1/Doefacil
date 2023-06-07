1a7fbed5ddf1b8803f3a8b9a2c5c75b9821231ba
GabrielCaetano13 <gabriel011depaulac@gmail.com>
2023-06-07 16:57:55 -0300

    controller das ações na pagina de doações

---
+++ Doefacil/Controllers/Actions.php
@@ -0,0 +1,20 @@
+<?php
+    $sql = "SELECT * FROM acoes ORDER BY expiration_date";
+    $stmt = $conn->query($sql);
+    $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    foreach($result as $row) {
+        $extension = strtolower(substr($row->thumbnail, -4));
+        echo "
+            <section class='box1'>
+                <h2 class='titleAction'>".$row->title."</h2>
+                <img class='imgAction' src='../assets/thumbnails/".$row->title.$extension."' alt='imagem da ação \"".$row->title."\"'>
+                <br>
+                <br>
+                <p class='historia1'>".$row->description."</p>
+                <br>
+                <p>".$row->action_creator."</p>
+            </section>
+        ";
+    }
+?>
